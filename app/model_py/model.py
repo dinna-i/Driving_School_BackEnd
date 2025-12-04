c0d43a9e10e52bc21cf5195269e4d5cdde72f84d
Nadeen-Gunathilake <nadeen.guna05@gmail.com>
2025-12-04 20:54:22 +0530

    machine learning model

--- app/model_py/model.py
+++ app/model_py/model.py
@@ -1,4 +1,5 @@
 import pandas as pd;
+import pickle;
 from sklearn.ensemble import RandomForestRegressor; 
 from sklearn.model_selection import train_test_split;
 from sklearn.metrics import mean_absolute_error;
@@ -26,6 +27,10 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
 model=RandomForestRegressor()
 model.fit(X_train,y_train)
+
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
 predictions=model.predict(X_test)
 
 mae = mean_absolute_error(y_test, predictions)
